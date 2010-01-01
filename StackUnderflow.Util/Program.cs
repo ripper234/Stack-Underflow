b47dc1828fe09b26822ee538e08d009bffc66e8a
Ron Gross <ron.gross@gmail.com>
2010-01-01 17:13:43 +0200

    View works

--- StackUnderflow.Util/Program.cs
+++ StackUnderflow.Util/Program.cs
@@ -2,6 +2,7 @@
 
 using System;
 using StackUnderflow.Bootstrap;
+using StackUnderflow.Model.Entities;
 using StackUnderflow.Persistence.Entities;
 using StackUnderflow.Persistence.Repositories;
 
@@ -17,12 +18,43 @@ namespace StackUnderflow.Util
             {
                 var container = Bootstrapper.Instance.GetContainer();
                 var userRepository = container.Resolve<IUserRepository>();
-                var user = new User {Name = "Ron"};
-                userRepository.Save(user);
+                var questionsRepository = container.Resolve<IQuestionsRepository>();
+
+                // save some users and questions
+                var ron = new User {Name = "Ron"};
+                var aya = new User { Name = "Aya" };
+                var miri = new User { Name = "Miri" };
+                userRepository.Save(ron);
+                userRepository.Save(aya);
+                userRepository.Save(miri);
+
+                var question1 = new Question
+                                    {
+                                        Author = ron,
+                                        Title = "Will this work?",
+                                        Text = "Well, will?",
+                                        UpdateDate = DateTime.Now
+                                    };
+                var question2 = new Question
+                                    {
+                                        Author = aya,
+                                        Title = "Watch star trek?",
+                                        Text = "Can we do it?",
+                                        UpdateDate = DateTime.Now.Subtract(TimeSpan.FromMinutes(5))
+                                    };
+                questionsRepository.Save(question1);
+                questionsRepository.Save(question2);
+
+                var questions = questionsRepository.GetNewestQuestions(10);
+                Console.WriteLine(string.Format("Got {0} questions", questions.Length));
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
+                while (e != null)
+                {
+                    Console.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
+                    e = e.InnerException;
+                }
             }
         }
     }
--- StackUnderflow.Web.Ui/Views/Home/Index.aspx
+++ StackUnderflow.Web.Ui/Views/Home/Index.aspx
@@ -1,12 +1,58 @@
-﻿<%@ Page Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<IEnumerable<StackUnderflow.Model.Entities.Question>>" %>
 
-<asp:Content ID="indexTitle" ContentPlaceHolderID="TitleContent" runat="server">
-    Home Page
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Index
 </asp:Content>
 
-<asp:Content ID="indexContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%= Html.Encode(ViewData["Message"]) %></h2>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Index</h2>
+
+    <table>
+        <tr>
+            <th></th>
+            <th>
+                Id
+            </th>
+            <th>
+                Title
+            </th>
+            <th>
+                Text
+            </th>
+            <th>
+                UpdateDate
+            </th>
+        </tr>
+
+    <% foreach (var item in Model) { %>
+    
+        <tr>
+            <td>
+                <%= Html.ActionLink("Edit", "Edit", new { /* id=item.PrimaryKey */ }) %> |
+                <%= Html.ActionLink("Details", "Details", new { /* id=item.PrimaryKey */ })%>
+            </td>
+            <td>
+                <%= Html.Encode(item.Id) %>
+            </td>
+            <td>
+                <%= Html.Encode(item.Title) %>
+            </td>
+            <td>
+                <%= Html.Encode(item.Text) %>
+            </td>
+            <td>
+                <%= Html.Encode(String.Format("{0:g}", item.UpdateDate)) %>
+            </td>
+        </tr>
+    
+    <% } %>
+
+    </table>
+
     <p>
-        To learn more about ASP.NET MVC visit <a href="http://asp.net/mvc" title="ASP.NET MVC Website">http://asp.net/mvc</a>.
+        <%= Html.ActionLink("Create New", "Create") %>
     </p>
+
 </asp:Content>
+
