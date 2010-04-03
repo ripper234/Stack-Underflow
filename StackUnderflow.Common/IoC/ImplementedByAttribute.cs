8f6ae6ca9080259ef984beb48893dff8f9eb6157
unknown <ron@.(none)>
2010-04-03 15:29:29 +0300

    Remove Delver namespace

--- StackUnderflow.Common/IoC/ImplementedByAttribute.cs
+++ StackUnderflow.Common/IoC/ImplementedByAttribute.cs
@@ -7,35 +7,32 @@ using Castle.Core;
 
 namespace StackUnderflow.IoC
 {
-    namespace Delver.Framework.IoC
-    {
-        /// <summary>
-        ///   Specifying hints for container registration.
-        ///   When used in conjunction with the AutoWireServicesIn extension method - allow overriding the defaults
-        /// </summary>
-        [AttributeUsage(AttributeTargets.Interface, Inherited = false)]
-        public class ImplementedByAttribute : Attribute
-        {
-            public ImplementedByAttribute(Type serviceType)
-            {
-                ServiceType = serviceType;
+	/// <summary>
+	///   Specifying hints for container registration.
+	///   When used in conjunction with the AutoWireServicesIn extension method - allow overriding the defaults
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Interface, Inherited = false)]
+	public class ImplementedByAttribute : Attribute
+	{
+		public ImplementedByAttribute(Type serviceType)
+		{
+			ServiceType = serviceType;
 
-                // default
-                Lifestyle = LifestyleType.Singleton;
-            }
+			// default
+			Lifestyle = LifestyleType.Singleton;
+		}
 
-            /// <summary>
-            ///   The component's lifestyle. The default is Singleton
-            /// </summary>
-            public LifestyleType Lifestyle { get; set; }
+		/// <summary>
+		///   The component's lifestyle. The default is Singleton
+		/// </summary>
+		public LifestyleType Lifestyle { get; set; }
 
-            /// <summary>
-            ///   The type of the service (usually an interface) this component is implementing
-            /// </summary>
-            public Type ServiceType { get; set; }
-        }
-    }
-
+		/// <summary>
+		///   The type of the service (usually an interface) this component is implementing
+		/// </summary>
+		public Type ServiceType { get; set; }
+	}
+ 
     [MyAttribute]
     public class MyAttribute : Attribute
     {
